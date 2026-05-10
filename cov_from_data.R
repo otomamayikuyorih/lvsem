943cd6b2a236c6487964b5a1c1f6a808a27f1b21
otomamayikuyorih <al-037d912n@gsuite.kobe-u.ac.jp>
2026-05-10 19:45:36 +0900

    Delete cov_from_data.R

--- cov_from_data.R
+++
@@ -1,9 +0,0 @@
-## データ行列 X から共分散行列 S を作るヘルパー
-cov_from_data <- function(X, use_correlation = FALSE) {
-  X <- scale(X, center = TRUE, scale = !use_correlation)
-  if (use_correlation) {
-    return(cor(X))
-  } else {
-    return(cov(X))
-  }
-}
